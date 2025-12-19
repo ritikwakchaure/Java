78b4cc23c53035dad379f1e340ea62a3d99018aa
Ritik Wakchaure <rutikwakchaure805@gmail.com>
2025-12-19 14:38:10 +0530

    first commit

---
+++ Day-4_assignment/src/com/app/geometry/Point.java
@@ -0,0 +1,22 @@
+package com.app.geometry;
+
+public class Point {
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public String getDetails() {
+        return "(" + x + ", " + y + ")";
+    }
+
+    // distance between this point and another point
+    public double calculateDistance(Point other) {
+        int dx = this.x - other.x;
+        int dy = this.y - other.y;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+}
